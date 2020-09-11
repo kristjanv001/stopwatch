0b1c172371c3cd1b5402ede1f833932a99e26a59
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2020-09-11 08:59:04 +0300

    basic stopwatch done

--- app.js
+++ app.js
@@ -1,74 +1,73 @@
 const startBtn = document.getElementById("start-btn");
 const startBtnText = document.getElementById("start-btn-text");
-const timeMs = document.getElementById("time-ms");
-const timeSec = document.getElementById("time-sec");
-const timeMin = document.getElementById("time-min");
+const timeText = document.getElementById("time-text");
 
-startBtn.addEventListener("click", (e) => {
-  toggleTimer();
-});
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-let timerId = null;
-let ms = 0;
-let sec = 0;
-let min = 0;
+let stopwatchId = null;
 
-function toggleTimer() {
-  console.log(startBtnText.innerText);
-  toggleBtnClassName();
-  toggleBtnText();
-  if (!timerId) {
-    timerId = setInterval(() => {
-      ms += 1;
-      renderTime();
-    }, 10);
+timeText.innerText = `00:00:00`;
+
+let secondsOnDisplay = 0;
+let minutesOnDisplay = 0;
+let hoursOnDisplay = 0;
+
+// stopwatch
+function startStopwatch() {
+  seconds++;
+  console.log("seconds:", seconds, "minutes:", minutes);
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+  }
+
+  if (minutes / 60 === 1) {
+    minutes = 0;
+    hours++;
+  }
+
+  renderStopwatch();
+}
+
+// render
+function renderStopwatch() {
+  secondsToDisplay = seconds;
+  minutesToDisplay = minutes;
+  hoursToDisplay = hours;
+
+  seconds < 10
+    ? (secondsToDisplay = `${0}${secondsToDisplay}`)
+    : (secondsToDisplay = `${secondsToDisplay}`);
+
+  minutes < 10
+    ? (minutesToDisplay = `${0}${minutesToDisplay}`)
+    : (minutesToDisplay = `${minutesToDisplay}`);
+
+  hours < 10
+    ? (hoursToDisplay = `${0}${hoursToDisplay}`)
+    : (hoursToDisplay = `${hoursToDisplay}`);
+
+  timeText.innerText = `${hoursToDisplay}:${minutesToDisplay}:${secondsToDisplay}`;
+}
+
+// event listener
+startBtn.addEventListener("click", () => {
+  if (!stopwatchId) {
+    stopwatchId = setInterval(startStopwatch, 10);
+    startBtn.className = "start-btn-started";
+    startBtnText.innerText = "STOP";
+    console.log("stopwatch started");
   } else {
-    ms = 0;
-    sec = 0;
-    min = 0;
+    clearInterval(stopwatchId);
+    stopwatchId = null;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    startBtn.className = "start-btn-stopped";
+    startBtnText.innerText = "START";
     console.log("stopwatch stopped");
-    clearInterval(timerId);
-    timerId = null;
   }
-}
-
-function renderTime() {
-  timeSec.innerText = sec;
-  timeMin.innerText = min;
-  if (ms === 100) {
-    timeSec.innerText++;
-    sec++;
-    ms = 0;
-  }
-  if (sec === 60) {
-    timeMin.innerText++;
-    min++;
-    sec = 0;
-  }
-}
-
-function toggleBtnClassName() {
-  if (startBtn.className === "start-btn-start") {
-    startBtn.className = "start-btn-stop";
-  } else {
-    startBtn.className = "start-btn-start";
-  }
-}
-
-function toggleBtnText() {
-  startBtnText.innerText === "STOP"
-    ? (startBtnText.innerText = "START")
-    : (startBtnText.innerText = "STOP");
-}
-
-// every time the minute counter gets to 60, change it to 0 and change the hour counter to 1
-
-// button class
-
-// double digits
-
-// push values to bottom and calc time sum
-
-// add clear button
-
-// push to local storage
+});
