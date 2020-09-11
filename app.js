fbbf08b555460e091a0ca213a76db6aeee4c6e85
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2020-09-11 12:33:23 +0300

    refactored - getDisplayTimes fn + added list functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const startBtn = document.getElementById("start-btn");
 const startBtnText = document.getElementById("start-btn-text");
 const timeText = document.getElementById("time-text");
+const timesList = document.getElementById("times-list");
 
 let seconds = 0;
 let minutes = 0;
@@ -10,14 +11,9 @@ let stopwatchId = null;
 
 timeText.innerText = `00:00:00`;
 
-let secondsOnDisplay = 0;
-let minutesOnDisplay = 0;
-let hoursOnDisplay = 0;
-
 // stopwatch
 function startStopwatch() {
   seconds++;
-  console.log("seconds:", seconds, "minutes:", minutes);
 
   if (seconds / 60 === 1) {
     seconds = 0;
@@ -28,39 +24,35 @@ function startStopwatch() {
     minutes = 0;
     hours++;
   }
-
   renderStopwatch();
+}
+
+// get display times
+function getDisplayTimes(time) {
+  let timeToDisplay = time;
+  time < 10
+    ? (timeToDisplay = `${0}${timeToDisplay}`)
+    : (timeToDisplay = `${timeToDisplay}`);
+  return timeToDisplay;
 }
 
 // render
 function renderStopwatch() {
-  secondsToDisplay = seconds;
-  minutesToDisplay = minutes;
-  hoursToDisplay = hours;
-
-  seconds < 10
-    ? (secondsToDisplay = `${0}${secondsToDisplay}`)
-    : (secondsToDisplay = `${secondsToDisplay}`);
-
-  minutes < 10
-    ? (minutesToDisplay = `${0}${minutesToDisplay}`)
-    : (minutesToDisplay = `${minutesToDisplay}`);
-
-  hours < 10
-    ? (hoursToDisplay = `${0}${hoursToDisplay}`)
-    : (hoursToDisplay = `${hoursToDisplay}`);
-
-  timeText.innerText = `${hoursToDisplay}:${minutesToDisplay}:${secondsToDisplay}`;
+  timeText.innerText = `${getDisplayTimes(hours)}:${getDisplayTimes(
+    minutes
+  )}:${getDisplayTimes(seconds)}`;
 }
 
 // event listener
 startBtn.addEventListener("click", () => {
   if (!stopwatchId) {
-    stopwatchId = setInterval(startStopwatch, 10);
+    stopwatchId = setInterval(startStopwatch, 1000);
     startBtn.className = "start-btn-started";
     startBtnText.innerText = "STOP";
     console.log("stopwatch started");
   } else {
+    pushResultToTimesList({ seconds: seconds, minutes: minutes, hours: hours });
+    renderTimesList();
     clearInterval(stopwatchId);
     stopwatchId = null;
     seconds = 0;
@@ -71,3 +63,33 @@ startBtn.addEventListener("click", () => {
     console.log("stopwatch stopped");
   }
 });
+
+// PART II
+
+const results = [];
+
+// push results fn
+function pushResultToTimesList(result) {
+  results.push(result);
+}
+
+// render list fn
+function renderTimesList() {
+  let li = `
+    <li>
+        <span class="result-time">${getDisplayTimes(hours)}:${getDisplayTimes(
+    minutes
+  )}:${getDisplayTimes(seconds)}</span>
+        <span class="remove">&#x2a2f;</span>
+    </li>`;
+
+  timesList.insertAdjacentHTML("afterbegin", li);
+}
+
+/* timesList.appendChild(`
+    <li>
+        <span class="result-time">${result.hours}:${result.minutes}:${result.seconds}</span>
+        <span class="remove">&#x2a2f;</span>
+    </li>`); */
+
+// let li = document.createElement("li");
